04f0d880033d605074cf853a6302844ca957f1cc
METADIL MÓVEIS - Caio Lesnock <caio@metadil.com.br>
2020-09-25 16:31:54 -0300

    Remove dist folder

--- dist/configs.d.ts
+++
@@ -1,5 +0,0 @@
-declare const _default: {
-    persist: boolean;
-    allowExistingData: boolean;
-};
-export default _default;
--- dist/persist.d.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Get store data from localStorage
- */
-export declare function getData(): Map<string, any>;
-/**
- * Save store data in localStorage
- * @param data
- */
-export declare function saveData(data: Map<any, any>): void;
